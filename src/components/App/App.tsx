38e5f727d8e1bbf9adffc91daea7a4831566efea
Allyson Hansel <allysongithub21@gmail.com>
2022-11-22 16:50:05 -0300

    Inputs funcionando e botão também

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,50 @@
-import React from 'react';
-import './App.modules.scss'
+import React, { useState, ChangeEvent } from "react";
+import "./App.modules.scss";
 
 function App() {
+
+  const [altura, setAltura] = useState(0);
+  const [peso, setPeso] = useState(0);
+
+  const debug = (event: ChangeEvent<HTMLFormElement>) => {
+    console.log(altura, peso);
+    event.preventDefault()
+  };
+
+  function handleChangePeso(event: ChangeEvent<HTMLInputElement>){
+    const novoValor = event.target.valueAsNumber;
+    return setPeso(novoValor)
+  }
+
+  function handleChangeAltura(event: ChangeEvent<HTMLInputElement>){
+    const novoValor = event.target.valueAsNumber;
+    return setAltura(novoValor)
+  }
+
   return (
-    <form action="POST">
+    <form action="POST" onSubmit={debug}>
       <div>
-        Altura <input type="text" placeholder='Insira sua Altura'/>
-        Peso <input type="text" placeholder='Insira seu Peso' />
+        Altura (m)
+        <input
+          type="number"
+          min="0"
+          max="3.00"
+          step="0.01"
+          placeholder="Insira sua Altura em m" value={altura}
+          onChange={handleChangeAltura}
+        />
+        Peso (kg)
+        <input
+          type="number"
+          min="0"
+          max="500"
+          placeholder="Insira seu Peso em kg" value={peso}
+          onChange={handleChangePeso}
+        />
       </div>
-      <button type="submit">Calcular</button>
+      <button type="submit">
+        Calcular
+      </button>
     </form>
   );
 }

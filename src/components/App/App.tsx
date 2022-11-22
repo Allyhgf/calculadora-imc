58c8fbca8f6fe4ff740afa0a1c59df4668c8a4d7
Allyson Hansel <allysongithub21@gmail.com>
2022-11-22 17:32:02 -0300

    STYLING PELA METADE.

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,50 +1,53 @@
 import React, { useState, ChangeEvent } from "react";
-import "./App.modules.scss";
+import style from "./App.module.scss";
 
 function App() {
-
   const [altura, setAltura] = useState(0);
   const [peso, setPeso] = useState(0);
 
   const debug = (event: ChangeEvent<HTMLFormElement>) => {
-    alert(peso/altura^2)
-    event.preventDefault()
+    alert(`Seu valor de IMC é: ${(peso / altura) ^ 2} `);
+    event.preventDefault();
   };
 
-  function handleChangePeso(event: ChangeEvent<HTMLInputElement>){
+  function handleChangePeso(event: ChangeEvent<HTMLInputElement>) {
     const novoValor = event.target.valueAsNumber;
-    return setPeso(novoValor)
+    return setPeso(novoValor);
   }
 
-  function handleChangeAltura(event: ChangeEvent<HTMLInputElement>){
+  function handleChangeAltura(event: ChangeEvent<HTMLInputElement>) {
     const novoValor = event.target.valueAsNumber;
-    return setAltura(novoValor)
+    return setAltura(novoValor);
   }
 
   return (
     <form action="POST" onSubmit={debug}>
-      <div>
-        Altura (m)
+      <div className={style.divWrapper}>
+        <p className={style.instrucoes}>Insira sua altura em m</p>
         <input
+          className={style.dadosUsuario}
           type="number"
           min="0"
           max="3.00"
           step="0.01"
-          placeholder="Insira sua Altura em m" value={altura}
+          value={altura}
           onChange={handleChangeAltura}
         />
-        Peso (kg)
+        <p className={style.instrucoes}>Insira seu peso em kg</p>
         <input
+          className={style.dadosUsuario}
           type="number"
           min="0"
           max="500"
-          placeholder="Insira seu Peso em kg" value={peso}
+          value={peso}
           onChange={handleChangePeso}
         />
+        <div className={style.centerBotao}>
+          <button type="submit" className={style.botaoSubmit}>
+            Calcular
+          </button>
+        </div>
       </div>
-      <button type="submit">
-        Calcular
-      </button>
     </form>
   );
 }
